4b35dbca7ccb3b0cc39b26605e0a113e87e0bf88
kelvin <kelvinfaquetti1@gmail.com>
2024-06-11 17:32:30 -0300

    11/06

---
+++ src/java/model/bean/Endereco.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+/**
+ *
+ * @author Senai
+ */
+public class Endereco {
+    private int idEndereco;
+    private String estado;
+    private String cidade;
+    private int cep;
+    private String bairro;
+    private String rua;
+    private int numero;
+
+    public int getIdEndereco() {
+        return idEndereco;
+    }
+
+    public void setIdEndereco(int idEndereco) {
+        this.idEndereco = idEndereco;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Endereco(int idEndereco, String estado, String cidade, int cep, String bairro, String rua, int numero) {
+        this.idEndereco = idEndereco;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    public Endereco() {
+    }
+}
